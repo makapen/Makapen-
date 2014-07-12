5ecdceecfd3c9d818a85806d2fc42d5c46870bcb
tyleriguchi <tguchi3@gmail.com>
2014-07-11 18:05:05 -0700

    scroll funcion in, waypoints working on

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,28 @@
-(function () {
-  'use strict';
-
-  function nav_disappear() {
-    var screen_location = window.screenTop;
-    var window_height = window.innerHeight;
-    var nav = document.querySelector('nav-bar');
-    if (screen_location > window_height) {
-      nav.style.display = "none";
-      alert('something');
-    }
+$(function() {
+  function scroll() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
   }
 
-  window.addEventListener('scroll', nav_disappear);
-})();
+  var logoMain = $('services');
+  var navBar = document.getElementById('nav-bar');
+  var navIcon = document.getElementById('nav-icon');
+
+  logoMain.waypoint(function() {
+    navBar.style.display = "none";
+    navIcon.style.display = "block";
+    console.log('ran')
+  }, {offset: 1000});
+
+  scroll();
+});
