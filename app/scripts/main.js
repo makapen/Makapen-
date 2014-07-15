805eda551ffae962878bcdbd0271e28a9a7b93fc
tyleriguchi <tguchi3@gmail.com>
2014-07-14 18:55:50 -0700

    icon style changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,9 @@
 $(function() {
-  function scroll() {
+  'use strict';
+
+  var app = app || {};
+
+  app.scroll = function() {
     $('a[href*=#]:not([href=#])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
@@ -12,7 +16,33 @@ $(function() {
         }
       }
     });
-  }
+  };
 
-  scroll();
+  app.nav = function() {
+    var logoMain = $('#services');
+    var nav_bar = document.querySelector('#nav-bar');
+    var nav_icon = document.querySelectorAll('.nav-icon__content')[0];
+
+    $('#services').waypoint({
+      offset: '0%',
+      handler: function(direction) {
+        if (direction === "down") {
+          console.log('removed nav bar');
+          nav_icon.style.position = "relative";
+          nav_icon.style.top = '20px !important';
+          nav_icon.style.right = '20px !important';
+        }
+        if (direction === "up") {
+          console.log('added nav bar');
+        }
+      }
+    });
+  };
+
+  app.init = function() {
+    app.scroll();
+    app.nav();
+  };
+
+  app.init();
 });
