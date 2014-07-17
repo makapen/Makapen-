a7ef9ba1153b8dbb1f41457963f9af97025d24c5
tyleriguchi <tguchi3@gmail.com>
2014-07-16 17:17:41 -0700

    nav changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,42 +19,41 @@ $(function() {
   };
 
   fn.nav = function() {
-    var services = $('#services');
-    var logo = $('#logo');
-    var nav_bar = $('#nav-bar');
-    var nav_ham = $('.nav-ham__icon');
-    var nav_drawer = $('.nav-drawer');
-    console.log(nav_bar.height());
-    logo.waypoint({
+    var $logo = $('#logo');
+    var $nav_bar = $('#nav-bar');
+    var $nav_ham = $('.nav-ham__icon');
+    var $nav_drawer = $('.nav-drawer');
+    $logo.waypoint({
       offset: function() {
-        return -nav_bar.height();
+        return -$nav_bar.height();
       },
       handler: function(direction) {
         if (direction === "down") {
-          nav_ham.addClass('nav-ham__icon--sticky');
+          $nav_ham.addClass('nav-ham__icon--sticky');
         }
-        else if (direction === "up" && nav_drawer.hasClass('nav-drawer')) {
-          nav_ham.removeClass('nav-ham__icon--sticky');
-          nav_drawer.removeClass('nav-drawer--active');
+        // Close the drawer if it's open and your moving up
+        else if (direction === "up" && $nav_drawer.hasClass('nav-drawer--active')) {
+          $nav_ham.removeClass('nav-ham__icon--sticky');
+          $nav_drawer.removeClass('nav-drawer--active');
         }
         else if (direction === "up") {
-          nav_ham.removeClass('nav-ham__icon--sticky');
+          $nav_ham.removeClass('nav-ham__icon--sticky');
         }
       }
     });
   };
 
   fn.drawer = function() {
-    var nav_ham = $('.nav-ham__icon');
-    var nav_drawer = $('.nav-drawer');
+    var $nav_ham = $('.nav-ham__icon');
+    var $nav_drawer = $('.nav-drawer');
 
-    nav_ham.on('click', function(e) {
-      if (nav_drawer.hasClass('nav-drawer--active')) {
-        nav_drawer.removeClass('nav-drawer--active');
-      }
-      else {
-        nav_drawer.addClass('nav-drawer--active');
-      }
+    $nav_ham.on('click', function(e) {
+        if ($nav_drawer.hasClass('nav-drawer--active')) {
+          $nav_drawer.removeClass('nav-drawer--active');
+        }
+        else {
+          $nav_drawer.addClass('nav-drawer--active');
+        }
     });
   };
 
