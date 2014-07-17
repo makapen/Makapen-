b1f0bd35a249b942bd377e445d1f8aa727923a21
tyleriguchi <tguchi3@gmail.com>
2014-07-17 15:22:23 -0700

    add mklogo to navbar, add padding to portfolio

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,30 +18,45 @@ $(function() {
     });
   };
 
+  // Show nav icons using waypoints
   fn.nav = function() {
     var $logo = $('#logo');
     var $nav_bar = $('#nav-bar');
     var $nav_ham = $('.nav-ham__icon');
     var $nav_drawer = $('.nav-drawer');
-    $logo.waypoint({
-      offset: function() {
-        return -$nav_bar.height();
-      },
-      handler: function(direction) {
-        if (direction === "down") {
-          $nav_ham.addClass('nav-ham__icon--sticky');
+    var $nav_mk = $('.nav-mk__icon');
+
+    // If mobile, show nav icons off the bat, hack
+    if (window.innerWidth > 480) {
+      $logo.waypoint({
+        offset: function() {
+          return -$nav_bar.height();
+        },
+        handler: function(direction) {
+          if (direction === "down") {
+            $nav_ham.addClass('nav-ham__icon--sticky');
+            $nav_mk.addClass('nav-mk__icon--sticky');
+            // $nav_bar.css('visibility', 'hidden');
+          }
+          // Close the drawer if it's open and your moving up
+          else if (direction === "up" && $nav_drawer.hasClass('nav-drawer--active')) {
+            $nav_ham.removeClass('nav-ham__icon--sticky');
+            $nav_mk.removeClass('nav-mk__icon--sticky');
+            $nav_drawer.removeClass('nav-drawer--active');
+            // $nav_bar.css('visibility', 'visible');
+          }
+          // Scrolled up
+          else {
+            $nav_ham.removeClass('nav-ham__icon--sticky');
+            $nav_mk.removeClass('nav-mk__icon--sticky');
+            // $nav_bar.css('visibility', 'visible');
+          }
         }
-        // Close the drawer if it's open and your moving up
-        else if (direction === "up" && $nav_drawer.hasClass('nav-drawer--active')) {
-          $nav_ham.removeClass('nav-ham__icon--sticky');
-          $nav_drawer.removeClass('nav-drawer--active');
-        }
-        // Scrolled up
-        else {
-          $nav_ham.removeClass('nav-ham__icon--sticky');
-        }
-      }
-    });
+      });
+    }
+    else {
+      $nav_ham.addClass('nav-ham__icon--sticky');
+    }
   };
 
   fn.drawer = function() {
