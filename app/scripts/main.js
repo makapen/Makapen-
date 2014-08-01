f0ecff1635d5ef3bcfbbbde6a06ed256f1cb9ed3
tyleriguchi <tguchi3@gmail.com>
2014-08-01 15:09:17 -0700

    fade in nav on scroll up, minor class name changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -35,27 +35,36 @@ $(document).ready(function() {
           return -$nav_bar.height();
         },
         handler: function(direction) {
+          var time_delay = 450;
+
           if (direction === "down") {
-            $nav_ham.addClass('fade');
-            $nav_mk.addClass('fade');
+            $nav_ham.addClass('fade-in');
+            $nav_mk.addClass('fade-in');
+            $nav_bar.addClass('fade-out');
           }
           // Close the drawer if it's open and your moving up
           else if (direction === "up" && $nav_drawer.hasClass('nav-drawer--active')) {
-            $nav_ham.removeClass('fade');
-            $nav_mk.removeClass('fade');
+            $nav_ham.removeClass('fade-in');
+            $nav_mk.removeClass('fade-in');
             $nav_ham.removeClass('nav-ham__icon--open');
             $nav_drawer.removeClass('nav-drawer--active');
+            setTimeout(function() {
+              $nav_bar.removeClass('fade-out').delay(3000);
+            }, time_delay);
           }
           // Scrolled up
           else {
-            $nav_ham.removeClass('fade');
-            $nav_mk.removeClass('fade');
+            $nav_ham.removeClass('fade-in');
+            $nav_mk.removeClass('fade-in');
+            setTimeout(function() {
+              $nav_bar.removeClass('fade-out').delay(3000);
+            }, time_delay);
           }
         }
       });
     }
     else {
-      $nav_ham.addClass('fade');
+      $nav_ham.addClass('fade-in');
     }
   };
 
@@ -64,13 +73,14 @@ $(document).ready(function() {
     var $nav_drawer = $('.nav-drawer');
     var $nav_wrap = $('.nav-ham__wrapper');
     var $nav_anchor = $('.nav-mk__icon').parent();
-    console.log($nav_anchor);
+
     $nav_wrap.on('click', $nav_ham, function(e) {
       e.preventDefault();
       $nav_ham.toggleClass('nav-ham__icon--open');
       $nav_drawer.toggleClass('nav-drawer--active');
     });
   };
+
   fn.init = function() {
     fn.scroll();
     fn.nav();
